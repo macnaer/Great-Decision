47c073603047eb120a478a9d05ba1329c3d64636
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-05-27 20:51:45 +0300

    Add some code....

--- app/pages/urls.py
+++ app/pages/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('about', views.about, name="about"),
+    path('we_offer', views.we_offer, name="we_offer"),
+    path('apartment', views.apartment, name="apartment"),
 ]

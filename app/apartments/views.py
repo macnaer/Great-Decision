3c72d515d41d47e20a55b19a99cd7f498eb637bb
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-06-03 19:28:57 +0300

    estatelist and pagination

--- app/apartments/views.py
+++ app/apartments/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Apartments
 
 
@@ -6,7 +7,10 @@ def index(request):
     apartments = Apartments.objects.order_by(
         "-list_date").filter(is_publish=True)
 
+    paginator = Paginator(apartments, 5)
+    page = request.GET.get('page')
+    pagged_apartments = paginator.get_page(page)
     context = {
-        'apartments': apartments
+        'apartments': pagged_apartments
     }
     return render(request, "pages/apartment.html", context)
--- app/app/settings.py
+++ app/app/settings.py
@@ -128,3 +128,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'app/static')
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
+MEDIA_URL = '/uploads/'
